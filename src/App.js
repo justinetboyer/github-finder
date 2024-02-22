2763d80cac4e4318afe1ff0de285c1d53db66529
Justine Boyer <160619295+justinetboyer@users.noreply.github.com>
2024-02-22 17:16:35 +0200

    adding nav component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, {Component} from 'react';
+import Navbar from './components/layout/Navbar';
+import './App.css'
 
 class App extends Component{
   render(){
     return (
       <div className="App">
-        <h1>hey</h1>
+       <Navbar />
       </div>
-    );
+      );
+      // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, "hey"))
   }
 }
 
