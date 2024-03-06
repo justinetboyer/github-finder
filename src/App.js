f9c9db35c6bbdef528cec6c46f0d435261926f42
Justine Boyer <160619295+justinetboyer@users.noreply.github.com>
2024-03-06 15:25:20 +0200

    Create Reducer & Actions

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, Fragment } from "react";
+import React, { useState, Fragment, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
@@ -18,26 +18,23 @@ const App = () => {
   const [repos, setRepos] = useState([]);
   const [loading, setLoading] = useState(false);
   const [alert, setAlert] = useState(null);
+  const [allUsers, setAllUsers] = useState([]);
 
-  // async componentDidMount() {
-  //   this.setState({ loading: true });
-  //   const res = await axios.get(
-  //     `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-  //   );
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      const res = await axios.get(
+        `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      );
+      setUsers(res.data);
+      setAllUsers(res.data);
+      setLoading(false);
+    };
 
-  //   this.setState({ users: res.data, loading: false });
-  // }
+    fetchData();
+  }, []);
 
-  const searchUsers = async (text) => {
-    setLoading(true);
 
-    const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-
-    setUsers(res.data.items);
-    setLoading(false);
-  };
 
   const getUser = async (username) => {
     setLoading(true);
@@ -59,7 +56,7 @@ const App = () => {
   };
 
   const clearUsers = () => {
-    setUsers([]);
+    setUsers(allUsers);
     setLoading(false);
   };
 
@@ -83,7 +80,6 @@ const App = () => {
                 render={(props) => (
                   <Fragment>
                     <Search
-                      searchUsers={searchUsers}
                       clearUsers={clearUsers}
                       showClear={users.length > 0 ? true : false}
                       setAlert={showAlert}
